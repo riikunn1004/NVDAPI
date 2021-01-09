53c18802f854ef241b7f5a38dac7d25a8b4ffbe1
Ryo Okutani <riikunn@gmail.com>
2021-01-09 17:05:09 +0900

    関数化取りやめ。

--- get_cve_info.py
+++ get_cve_info.py
@@ -4,42 +4,6 @@ import json
 import argparse
 
 
-def get_description_data_value(json_data):
-    """NVDに記載されている脆弱性の説明(Current Description)を返す
-
-    Args:
-        json_data (dict): REST APIのレスポンスデータ(JSON形式)
-
-    Returns:
-        string: NVDに記載されている脆弱性の説明(Current Description)
-    """
-    return json_data['result']['CVE_Items'][0]['cve']['description']['description_data'][0]['value']
-
-
-def get_CWE_ID(json_data):
-    """CWE-IDを返す
-
-    Args:
-        json_data (dict): REST APIのレスポンスデータ(JSON形式)
-
-    Returns:
-        string: CWE-ID
-    """
-    return json_data['result']['CVE_Items'][0]['cve']['problemtype']['problemtype_data'][0]['description'][0]['value']
-
-
-def get_cvssv3_score(json_data):
-    """CVSS V3 Scoreを返す
-
-    Args:
-        json_data (dict): REST APIのレスポンスデータ(JSON形式)
-
-    Returns:
-        dict: CVSS V3 Score (BaseScoreやVectorString等も含む)
-    """
-    return json_data['result']['CVE_Items'][0]['impact']['baseMetricV3']
-
-
 def main():
     # コマンドライン引数Parse
     parser = argparse.ArgumentParser()
@@ -54,17 +18,17 @@ def main():
     json_data = json.loads(response.text)
 
     # NVDに記載されている脆弱性の説明(Current Description)を出力
-    description_data_value = get_description_data_value(json_data)
+    description_data_value = json_data['result']['CVE_Items'][0]['cve']['description']['description_data'][0]['value']
     print('------- Current Description -------')
     print(description_data_value)
 
     # CWE-IDを出力
-    CWE_ID = get_CWE_ID(json_data)
+    CWE_ID = json_data['result']['CVE_Items'][0]['cve']['problemtype']['problemtype_data'][0]['description'][0]['value']
     print('------- CWE-ID -------')
     print(CWE_ID)
 
     # CVSS V3 Scoreを出力
-    cvssv3_score = get_cvssv3_score(json_data)
+    cvssv3_score = json_data['result']['CVE_Items'][0]['impact']['baseMetricV3']
     print('------- CVSS V3 Score -------')
     print(cvssv3_score)
 
